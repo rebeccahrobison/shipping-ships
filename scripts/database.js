5f84a0196f7453b9a682e6aa73d550c851e89735
Rebecca Robison <rebeccahrobison@outlook.com>
2023-08-18 13:58:49 -0500

    Add shippingShips array and export

--- scripts/database.js
+++ scripts/database.js
@@ -12,6 +12,18 @@ const database = {
         { id: 4, name: "Downeaster Alexa", dockId: 2 },
         { id: 5, name: "The Orca", dockId: 2 },
         { id: 6, name: "Piper Maru", dockId: 4 }
+    ],
+    shippingShips: [
+        { id: 1, name: "Esso Atlantic", haulerId: 2 },
+        { id: 2, name: "Prairial", haulerId: 6 },
+        { id: 3, name: "Palais Royal", haulerId: 4 },
+        { id: 4, name: "Rivoli", haulerId: 1 },
+        { id: 5, name: "Champs Elysee", haulerId: 1 },
+        { id: 6, name: "Ever Ace", haulerId: 5 },
+        { id: 7, name: "Nissei Maru", haulerId: 4 },
+        { id: 8, name: "MSC Gulsun", haulerId: 3 },
+        { id: 9, name: "HMM Rotterdam", haulerId: 5 },
+        { id: 10, name: "CMA CGM Trocadero", haulerId: 3 }
     ]
 }
 
@@ -21,4 +33,8 @@ export const getDocks = () => {
 
 export const getHaulingShips = () => {
     return database.haulers.map(hauler = ({...hauler}))
+}
+
+export const getShippingShips = () => {
+    return database.shippingShips.map(ship = ({...ship}))
 }
