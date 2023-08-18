316bcd9a8b483ed29054249052fb567259a51fe8
Rebecca Robison <rebeccahrobison@outlook.com>
2023-08-18 15:02:42 -0500

    Add event listener and datasets

--- scripts/ShipHaulers.js
+++ scripts/ShipHaulers.js
@@ -1,12 +1,31 @@
-import { getHaulingShips } from "./database.js";
+import { getHaulingShips, getShippingShips } from "./database.js";
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.dataset.type === "hauler") {
+            let counter = 0
+            const shippingShips = getShippingShips()
+            for (const ship of shippingShips) {
+                if (ship.id === parseInt(itemClicked.dataset.id)) {
+                    counter++
+                }
+            }
+
+            window.alert()
+        }
+    }
+)
+
 
 export const HaulerList = () => {
     const haulers = getHaulingShips()
 
-    let haulersHTML = "<ul>"
+    let haulersHTML = `<ul class="haulers-list">`
 
     for (const hauler of haulers) {
-        haulersHTML += `<li>${hauler.id} ${hauler.name} ${hauler.dockId}</li>`
+        haulersHTML += `<li data-type="hauler" data-id="${hauler.id}">${hauler.name} ${hauler.dockId}</li>`
     }
 
     haulersHTML += `</ul>`
