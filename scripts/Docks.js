784a25bd6124e5215d4508a9ad790917385004d9
Rebecca Robison <rebeccahrobison@outlook.com>
2023-08-21 11:31:28 -0500

    Add event listener

--- scripts/Docks.js
+++ scripts/Docks.js
@@ -1,12 +1,60 @@
-import { getDocks } from "./database.js";
+import { getDocks, getHaulingShips } from "./database.js";
+
+const docks = getDocks()
+const haulers = getHaulingShips()
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        console.log(itemClicked.dataset.type)
+        if (itemClicked.dataset.type === "dock") {
+            const dockIdClicked = parseInt(itemClicked.dataset.id)
+            console.log(dockIdClicked)
+            let haulingShips = []
+            let dockCounter = 0
+            
+            for (const hauler of haulers) {
+                if (hauler.dockId === dockIdClicked){
+                    haulingShips.push(hauler.name)
+                    dockCounter++
+                }
+            }
+            
+            if (dockCounter === 0) {
+                window.alert(`The ${itemClicked.dataset.name} is currently unloading nothing`)
+            } else if (dockCounter === 1) {
+                window.alert(`The ${itemClicked.dataset.name} dock is currently unloading ${haulingShips[0]}`)
+            } else if (dockCounter > 1) {
+                let alertText = `The ${itemClicked.dataset.name} dock is currently unloading `
+                
+                let i =0
+                while (i < haulingShips.length - 1) {
+                    alertText += `${haulingShips[i]}, `
+                    i++
+                }
+                alertText += `${haulingShips[i]}`
+                window.alert(alertText)      
+            }
+                    
+                
+        }     
+    }
+)
 
 export const DockList = () => {
-    const docks = getDocks()
+
 
     let docksHTML = `<ul class='docks-list'>`
 
     for (const dock of docks) {
-        docksHTML += `<li data-id="${dock.id}" data-volume="${dock.volume}">${dock.location} </li>`
+        docksHTML += `<li 
+        data-type="dock"
+        data-name="${dock.location}"
+        data-id="${dock.id}" 
+        data-volume="${dock.volume}"
+        >${dock.location} 
+        </li>`
     }
 
     docksHTML += `</ul>`
