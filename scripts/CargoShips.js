f295788ce8d2a479b9b2848e2974176f72ae517c
Rebecca Robison <rebeccahrobison@outlook.com>
2023-08-21 10:08:10 -0500

    Add event listener. Add datasets to html

--- scripts/CargoShips.js
+++ scripts/CargoShips.js
@@ -1,12 +1,41 @@
-import { getShippingShips } from "./database.js";
+import { getShippingShips, getHaulingShips } from "./database.js";
 
+const cargoShips = getShippingShips()
+const haulingShips = getHaulingShips()
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        console.log(itemClicked.dataset.type)
+        console.log(parseInt(itemClicked.dataset.haulerid))
+        if (itemClicked.dataset.type === "cargoShip") {
+            const haulerIdClicked = parseInt(itemClicked.dataset.haulerid)
+            
+            let haulingShip = { name: "Incorrect" }
+            for (const hauler of haulingShips) {
+                if (hauler.id === haulerIdClicked) {
+                    haulingShip.name = hauler.name
+                }
+            }
+            window.alert(`${itemClicked.dataset.name} is being hauled by ${haulingShip.name}`)
+
+        }
+    }
+)
 export const cargoShipList = () => {
-    const cargoShip = getShippingShips()
+    
 
     let cargoShipHTML = `<ul class="cargoships-list">`
 
     for (const ship of cargoShips) {
-        cargoShipHTML += `<li>${ship.id} ${ship.name} ${ship.haulderId}</li>`
+        cargoShipHTML += `<li 
+                            data-type="cargoShip" 
+                            data-name="${ship.name}"
+                            data-id="${ship.id}" 
+                            data-haulerid="${ship.haulerId}"
+                            > ${ship.name}
+                         </li>`
     }
 
     cargoShipHTML += `</ul>`
